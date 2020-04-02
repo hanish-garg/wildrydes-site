70acc6926cc40e8aed6d3d197e52d5f61f46e65e
hanish-garg <hanishgarg@gmail.com>
2020-04-02 14:55:26 -0400

    configure cognito url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2 84065bciqcql6m20mmn2mbtre
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://d3cbsbysx2.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
